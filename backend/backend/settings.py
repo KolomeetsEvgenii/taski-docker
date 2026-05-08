c9ccadeea38506d8e2268cdcd498ab4435b7e9b1
Evgenii Kolomeets <kolomeetsevgenii@yandex.ru>
2026-05-08 15:47:02 +1000

    Add domain and IP to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '81.26.180.175', 'taski-77.ddns.net']
 
 # Application definition
 
